5ea2571559901a94baeb03be21106e9215ae7aae
satiscugcat <aniket.mishra@iitgn.ac.in>
2026-03-14 10:30:10 +0530

    Solution to ExerciseSheet 1

--- lean/LoVe/LoVe01_TypesAndTerms_ExerciseSheet.lean
+++ lean/LoVe/LoVe01_TypesAndTerms_ExerciseSheet.lean
@@ -26,24 +26,24 @@ while constructing a term. By hovering over `_`, you will see the current
 logical context. -/
 
 def I : α → α :=
-  fun a ↦ a
+  fun a => a
 
 def K : α → β → α :=
-  fun a b ↦ a
+  fun a b => a
 
 def C : (α → β → γ) → β → α → γ :=
-  sorry
+  fun abg b a => abg a b 
 
 def projFst : α → α → α :=
-  sorry
+  fun a₁ a₂ => a₁
 
 /- Give a different answer than for `projFst`. -/
 
 def projSnd : α → α → α :=
-  sorry
+  fun a₁ a₂ => a₂
 
 def someNonsense : (α → β → γ) → α → (α → γ) → β → γ :=
-  sorry
+  fun abg a ag b => ag a 
 
 
 /- ## Question 2: Typing Derivation
@@ -52,6 +52,23 @@ Show the typing derivation for your definition of `C` above, on paper or using
 ASCII or Unicode art. Start with an empty context. You might find the
 characters `–` (to draw horizontal bars) and `⊢` useful. -/
 
+/-
+
+  ————————————————————————————————————————————— Var  —————————————————————————————————————— Var 
+  abg : (α → β → γ), b: β, a: α  ⊢ abg : α → β → γ   abg : (α → β → γ), b: β, a: α  ⊢ a : α
+  ——————————————————————————————————————————————————————————————————————————————————————— App
+  abg : (α → β → γ), b: β, a: α  ⊢ abg a : β → γ   —————————————————————————————————————— Var
+                                                   abg : (α → β → γ), b: β, a: α  ⊢ b : β 
+  ——————————————————————————————————————————————————————————————————————————————————————— App
+  abg : (α → β → γ), b: β, a: α  ⊢ abg a b : γ
+  ——————————————————————————————————————————————————————— Fun
+  abg : (α → β → γ), b: β  ⊢  fun a : α ↦ abg a b : α → γ
+  ——————————————————————————————————————————————————————————————————— Fun
+  abg : (α → β → γ) ⊢ (fun b : β ↦ (fun a : α ↦ abg a b)) : β → α → γ
+  ———————————————————————————————————————————————————————————————————————————————————————————— Fun
+  ∅ ⊢ (fun abg : (α → β → γ) ↦ (fun b : β ↦ (fun a : α ↦ abg a b) )) : (α → β → γ) → β → α → γ
+
+-/
 -- write your solution in a comment here or on paper
 
 end LoVe
